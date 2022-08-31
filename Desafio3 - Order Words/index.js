96f0a50aeb0a66fea5d7e07859f5c1532d2a69ea
Matheus Camba <mhscamba@gmail.com>
2022-08-31 17:18:02 -0300

    Att

--- Desafio3 - Order Words/index.js
+++ Desafio3 - Order Words/index.js
@@ -47,3 +47,24 @@ function order(words) {
 
 let frase = "is2 Thi1s T4est 3a";
 order(frase);
+
+//Outras resolucoes
+// function order(words){
+
+//   return words.split(' ').sort(function(a, b){
+//       return a.match(/\d/) - b.match(/\d/);
+//    }).join(' ');
+// }
+
+// function order(words){
+//   var array = words.split(' ');
+//   var sortedArray = [];
+//   for(i = 0; i <= array.length; i++) {
+//     for(j = 0; j < array.length; j++) {
+//       if(array[j].indexOf(i) >= 0) {
+//         sortedArray.push(array[j]);
+//       }
+//     }
+//   }
+//   return sortedArray.join(' ');
+// }
